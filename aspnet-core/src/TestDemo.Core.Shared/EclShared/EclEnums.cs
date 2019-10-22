92655d72d437483a9b90519e351b938ba91cea6b
olabode33 <olabode33@gmail.com>
2019-10-22 11:57:11 +0100

    core: add enums

---
+++ aspnet-core/src/TestDemo.Core.Shared/EclShared/EclEnums.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDemo.EclShared
+{
+    public enum DataTypeEnum
+    {
+        String = 1, Datetime, Double
+    }
+
+    public enum FrameworkEnum
+    {
+        General, Wholesale, Retail, OBE
+    }
+
+    public enum ModuleEnum
+    {
+        General, Calibration, Input, Framework
+    }
+
+    public enum EclStatusEnum
+    {
+        Draft, Submitted, ApprovedAssumption, ComputedPD, ComputedSICR, ComputedEAD, ComputedLGD, ComputedECL, Completed, Closed
+    }
+
+    public enum UploadDocTypeEnum
+    {
+        General, LoanBook, PaymentSchedule
+    }
+
+    public enum UploadStatusEnum
+    {
+        Draft, Submitted, Approved, Rejected
+    }
+
+    public enum ResultSummaryTypeEnum
+    {
+        ByScenario, ByStage, ByProductType, BySegementStage
+    }
+
+    public enum EadInputGroupEnum
+    {
+        General, CreditConversionFactors, VariableInterestRateProjections, ExchangeRateProjections
+    }
+}
