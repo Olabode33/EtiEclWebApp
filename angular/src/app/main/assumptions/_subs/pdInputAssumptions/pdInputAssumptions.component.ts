dd1672e98929d11a829329128440af5a6551e80d
olabode33 <olabode33@gmail.com>
2019-12-25 09:07:14 +0100

    update s&p assumption view

--- angular/src/app/main/assumptions/_subs/pdInputAssumptions/pdInputAssumptions.component.ts
+++ angular/src/app/main/assumptions/_subs/pdInputAssumptions/pdInputAssumptions.component.ts
@@ -1,3 +1,4 @@
+import { filter } from 'rxjs/operators';
 import { PdInputAssumptionGroupEnum, GetAllPdAssumptionsDto, PdInputAssumptionDto, PdInputAssumptionMacroeconomicInputDto, PdInputAssumptionMacroeconomicProjectionDto, PdInputAssumptionNonInternalModelDto, PdInputAssumptionNplIndexDto, PdInputSnPCummulativeDefaultRateDto, DataTypeEnum } from './../../../../../shared/service-proxies/service-proxies';
 import { Component, OnInit, Injector } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
@@ -19,12 +20,19 @@ export class PdInputAssumptionsComponent extends AppComponentBase {
     etiPolicy: PdInputAssumptionDto[] = new Array();
     bestFit: PdInputAssumptionDto[] = new Array();
 
+    //S&P Mapping
+    pdSnpCumulativeDefaultRate: PdInputSnPCummulativeDefaultRateDto[] = new Array<PdInputSnPCummulativeDefaultRateDto>();
+    snpRatings: String[] = new Array();
+    snpYears: Number[] = new Array();
+    selectedRating = '';
+    selectedYear = -1;
+    selectedSnpCummulativeDefaultRate: PdInputSnPCummulativeDefaultRateDto[] = new Array();
+
 
     pdMacroeconomicInputAssumptions: PdInputAssumptionMacroeconomicInputDto[] = new Array<PdInputAssumptionMacroeconomicInputDto>();
     pdMacroeconomicProjectionsAssumptions: PdInputAssumptionMacroeconomicProjectionDto[] = new Array<PdInputAssumptionMacroeconomicProjectionDto>();
     pdNonInternalModelAssumptions: PdInputAssumptionNonInternalModelDto[] = new Array<PdInputAssumptionNonInternalModelDto>();
     pdNplIndexAssumptions: PdInputAssumptionNplIndexDto[] = new Array<PdInputAssumptionNplIndexDto>();
-    pdSnpCumulativeDefaultRate: PdInputSnPCummulativeDefaultRateDto[] = new Array<PdInputSnPCummulativeDefaultRateDto>();
 
     dataTypeEnum = DataTypeEnum;
 
@@ -43,15 +51,40 @@ export class PdInputAssumptionsComponent extends AppComponentBase {
     extractPdAssumptionGroups(): void {
         //Extract Pd input assumption to groups
         this.pdInputAssumptions = this.pdAssumptions.pdInputAssumption;
-        this.creditPds = this.pdInputAssumptions.filter(x => x.assumptionGroup === PdInputAssumptionGroupEnum.CreditPD);
-        this.etiPolicy = this.pdInputAssumptions.filter(x => x.assumptionGroup === PdInputAssumptionGroupEnum.CreditEtiPolicy);
-        this.bestFit = this.pdInputAssumptions.filter(x => x.assumptionGroup === PdInputAssumptionGroupEnum.CreditBestFit);
+        this.extract12MonthPds();
+
+        this.pdSnpCumulativeDefaultRate = this.pdAssumptions.pdInputSnPCummulativeDefaultRate;
+        this.extractSnPMappingForDropdown();
 
         this.pdMacroeconomicInputAssumptions = this.pdAssumptions.pdInputAssumptionMacroeconomicInput;
         this.pdMacroeconomicProjectionsAssumptions = this.pdAssumptions.pdInputAssumptionMacroeconomicProjections;
         this.pdNonInternalModelAssumptions = this.pdAssumptions.pdInputAssumptionNonInternalModels;
         this.pdNplIndexAssumptions = this.pdAssumptions.pdInputAssumptionNplIndex;
-        this.pdSnpCumulativeDefaultRate = this.pdAssumptions.pdInputSnPCummulativeDefaultRate;
+    }
+
+    extract12MonthPds(): void {
+        this.creditPds = this.pdInputAssumptions.filter(x => x.assumptionGroup === PdInputAssumptionGroupEnum.CreditPD);
+        this.etiPolicy = this.pdInputAssumptions.filter(x => x.assumptionGroup === PdInputAssumptionGroupEnum.CreditEtiPolicy);
+        this.bestFit = this.pdInputAssumptions.filter(x => x.assumptionGroup === PdInputAssumptionGroupEnum.CreditBestFit);
+    }
+
+    extractSnPMappingForDropdown(): void {
+        this.snpRatings = Array.from(new Set(this.pdSnpCumulativeDefaultRate.map((i) => i.rating)));
+        this.snpYears = Array.from(new Set(this.pdSnpCumulativeDefaultRate.map((i) => i.years)));
+        this.selectedSnpCummulativeDefaultRate = this.pdSnpCumulativeDefaultRate;
+    }
+
+    filterSnPCummulativeDefaultRate(): void {
+        console.log('Rating:' + this.selectedRating + ' Year: ' + this.selectedYear);
+        if (this.selectedRating !== '' && this.selectedYear !== -1) {
+            this.selectedSnpCummulativeDefaultRate = this.pdSnpCumulativeDefaultRate.filter(x => x.rating === this.selectedRating && x.years === this.selectedYear);
+        } else if (this.selectedRating === '' && this.selectedYear !== -1) {
+            this.selectedSnpCummulativeDefaultRate = this.pdSnpCumulativeDefaultRate.filter(x => x.years === this.selectedYear);
+        } else if (this.selectedRating !== '' && this.selectedYear === -1) {
+            this.selectedSnpCummulativeDefaultRate = this.pdSnpCumulativeDefaultRate.filter(x => x.rating === this.selectedRating);
+        } else {
+            this.selectedSnpCummulativeDefaultRate = this.pdSnpCumulativeDefaultRate;
+        }
     }
 
     hide(): void {
