69b8c0be1b0baa4bb46fdf2a2ebc7d0a02bdd1c5
olabode33 <olabode33@gmail.com>
2020-01-30 17:11:01 +0100

    add affiliate configuration

---
+++ aspnet-core/src/TestDemo.EntityFrameworkCore/Migrations/20200130160244_Extended-OU-for-AffiliateConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestDemo.Migrations
+{
+    public partial class ExtendedOUforAffiliateConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AbpOrganizationUnits",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "OverrideThreshold",
+                table: "AbpOrganizationUnits",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AbpOrganizationUnits");
+
+            migrationBuilder.DropColumn(
+                name: "OverrideThreshold",
+                table: "AbpOrganizationUnits");
+        }
+    }
+}
