d37b7e49bbe1baa8564b9e24d9f9b3906abb2166
olabode33 <olabode33@gmail.com>
2020-06-17 20:22:13 +0100

    add investment computation email alert

--- aspnet-core/src/TestDemo.Core/EclLibrary/Investment/RunInvestmentPostEclJob.cs
+++ aspnet-core/src/TestDemo.Core/EclLibrary/Investment/RunInvestmentPostEclJob.cs
@@ -1,11 +1,20 @@
 ﻿using Abp.BackgroundJobs;
 using Abp.Dependency;
+using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
+using Abp.Organizations;
 using Abp.Threading;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TestDemo.Authorization.Users;
+using TestDemo.Configuration;
 using TestDemo.EclLibrary.BaseEngine.Dtos;
+using TestDemo.EclShared;
+using TestDemo.EclShared.Emailer;
+using TestDemo.Investment;
 using TestDemo.InvestmentComputation;
 using TestDemo.Notifications;
 
@@ -15,21 +24,49 @@ namespace TestDemo.EclLibrary.Investment
     {
         private readonly IEclCustomRepository _investmentEclRepository;
         private readonly IAppNotifier _appNotifier;
+        private readonly IEclEngineEmailer _emailer;
+        private readonly IConfigurationRoot _appConfiguration;
+        private readonly IRepository<User, long> _userRepository;
+        private readonly IRepository<OrganizationUnit, long> _ouRepository;
+        private readonly IRepository<InvestmentEcl, Guid> _eclRepository;
 
         public RunInvestmentPostEclJob(IEclCustomRepository investmentEclRepository,
+            IEclEngineEmailer emailer,
+            IHostingEnvironment env,
+            IRepository<User, long> userRepository,
+            IRepository<OrganizationUnit, long> ouRepository,
+            IRepository<InvestmentEcl, Guid> eclRepository,
             IAppNotifier appNotifier)
         {
             _investmentEclRepository = investmentEclRepository;
             _appNotifier = appNotifier;
+            _emailer = emailer;
+            _appConfiguration = env.GetAppConfiguration();
+            _userRepository = userRepository;
+            _ouRepository = ouRepository;
+            _eclRepository = eclRepository;
         }
 
         //[UnitOfWork]
         public override void Execute(RunEclJobArgs args)
         {
             _investmentEclRepository.RunInvestmentPostOverrideEclStoredProcedure(args.EclId);
+            SendEmailAlert(args);
 
             // Send notification to user.
             //AsyncHelper.RunSync(() => _appNotifier.EclClosed(args.UserIdentifier));
+        }
+
+        private void SendEmailAlert(RunEclJobArgs args)
+        {
+            var user = _userRepository.FirstOrDefault(args.UserIdentifier.UserId);
+            var baseUrl = _appConfiguration["App:ClientRootAddress"];
+            var frameworkId = (int)FrameworkEnum.Investments;
+            string link = baseUrl + "/app/main/ecl/view/" + frameworkId.ToString() + "/";
+            var type = "Investment ECL Post-override";
+            var ecl = _eclRepository.FirstOrDefault(args.EclId);
+            var ou = _ouRepository.FirstOrDefault(ecl.OrganizationUnitId);
+            _emailer.SendEmailDataUploadCompleteAsync(user, type, ou.DisplayName, link);
         }
     }
 }
